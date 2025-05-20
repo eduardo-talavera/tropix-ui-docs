6eb6aa4ce123cca0ec6d5b8df8d5dc31d57d8bcf
Eduardo Talavera <edgar.talavera@ion.com.mx>
2025-05-20 05:05:06 -0600

    feat(themeRoot): update theme root

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,12 +1,11 @@
-
 import { ThemeProvider } from 'tropix-ui';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 
 // Default implementation, that you can customize
 export default function Root({children}) {
-  const isBrowser = useIsBrowser();
-  if (!isBrowser) return 'Loading...'
-  return <ThemeProvider>{children}</ThemeProvider>
+    const isBrowser = useIsBrowser(); 
+
+    return (isBrowser) && <ThemeProvider>{children}</ThemeProvider>
 }
 
 
