c6adc78a05cd669abb8d86af8ba128766929cfb6
Eduardo Talavera <edgar.talavera@ion.com.mx>
2025-05-20 21:04:47 -0600

    feat(theme): restore match media prefers

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'tropix-ui';
 
 
 // Default implementation, that you can customize
 export default function Root({children}) {
+
+     const [isClient, setIsClient] = useState(false);
+    
+      useEffect(() => {
+        setIsClient(true);
+      }, []);
+    
+      if (!isClient) return null;
  
     return <ThemeProvider>{children}</ThemeProvider>
 }
